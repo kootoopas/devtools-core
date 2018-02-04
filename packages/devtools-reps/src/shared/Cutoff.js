b2d447104f6cb1df5e1323758fe3f41a7d1fe42b
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2018-02-05 00:44:13 +0200

    Fix linting errors

--- packages/devtools-reps/src/shared/Cutoff.js
+++ packages/devtools-reps/src/shared/Cutoff.js
@@ -1,5 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // @flow
-import React, { Component } from "react";
+import { Component } from "react";
 const dom = require("react-dom-factories");
 const { span } = dom;
 const {
